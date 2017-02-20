6f16d6f69af9e0ca5a7a3e086d11ef5168ae727f
Joshua Harms <joshua@nozzlegear.com>
2017-02-20 11:43:04 -0600

    Initial commit 🚌

--- README.md
+++ README.md
@@ -1,2 +1,133 @@
 # gearworks-route
-The routing function used by Gearworks apps, complete with full TypeScript definitions.
+The routing function used by Gearworks apps, complete with full TypeScript definitions. This routing function simplifies Shopify webhook, proxy and request validation, JWT authentication, parameter validation and more. 
+
+[Gearworks](https://github.com/nozzlegear/gearworks) is the fastest way to get started with building Shopify apps!
+
+## Installation
+
+With [Yarn](https://github.com/yarnpkg/yarn):
+
+```shell
+yarn install gearworks-route
+```
+
+Or from [NPM](https://npmjs.com/package/gearworks-route):
+
+```shell
+npm install gearworks-route --save
+```
+
+## Importing
+
+Import gearworks-route via ES6 default import:
+
+```js
+import getRouter from "gearworks-route";
+```
+
+Or via Node's require:
+
+```js
+const getRouter = require("gearworks-route").default;
+```
+
+## Example
+
+Pass your Express app and a configuration object into the `getRouter` function, which will return a routing function that you can use to quickly configure routes:
+
+```js
+import * as express from "express";
+import getRouter from "gearworks-route";
+
+const app = express();
+const config = {
+    sealable_users_props: ["shopify_access_token"],
+    shopify_secret_key: "my shopify secret key used to validate Shopify requests",
+    iron_password: "My randomly generated password which will encrypt the sealable_users_props",
+    jwt_secret_key: "My randomly generated password which will sign JWT auth tokens",
+    userAuthIsValid: async (user) => {
+        // Use this function to tell the route whether the user's auth is now invalid by e.g. checking a cache or database.
+
+        return true;
+    }
+}
+const route = getRouter(expressApp, config);
+
+// Create a route
+route({
+    label: "Validate a Shopify webhook",
+    method: "post",
+    path: "/api/v1/webhooks/app-uninstalled",
+    validateShopifyWebhook: true,
+    handler: async function (req, res, next) {
+        // A user has uninstalled your Shopify app!
+        res.json({okay: true});
+
+        // All handlers must call next() when they're done.
+        return next();
+    }
+})
+```
+
+## Configuration
+
+The `getRouter` function expects you to pass in both an Express app, and a configuration object with the following values:
+
+| prop | type | required | description |
+|------|------|----------|-------------|
+| `shopify_secret_key` | string | true | Your Shopify app's secret key, used to validate Shopify requests. |
+| `iron_password` | string | true | A randomly-generated string used to encrypt and decrypt the properties in `sealable_user_props`. |
+| `jwt_secret_key` | string | true | A randomly-generated string used to sign JWT auth tokens. |
+| `sealable_user_props` | string array | false | A list of sensitive properties on your User object that should be encrypted and sealed by Iron. Usually you'd want to encrypt at minimum the user's Shopify access token. |
+| `auth_header_name` | string | false | The name of the header to check for auth tokens. Defaults to `gearworks_auth`. | 
+| `userAuthIsValid` | function | false | A function that receives the User object to check whether a user's auth is still valid (e.g. they uninstalled your app and should be logged out). Return true for valid, false for invalid. |
+
+## Route Configuration
+
+The `getRouter` function returns a `route` function, which you can use to quickly setup routes. It accepts a single parameter, an object with the following props:
+
+| prop | type | required | description |
+|------|------|----------|-------------|
+| `label` | string | false | A string which gives a quick summary of the route. Currently only used for developer convenience to quickly scan routes. |
+| `path` | string | true | The route's URL path. Can accept Express-style parameters, e.g. `/api/v1/orders/:id`. |
+| `method` | string | true | The route's request method. Must be either `get`, `post`, `put`, `delete`, `head` or `all`. Case-sensitive, must be all lowercase. |
+| `handler` | function | true | The route's handler, a function which accepts  `req`, `res` and `next` parameters. Can be async. All handlers must call `next()` to end the request. |
+| `cors` | boolean | false | A flag which enables Cross-Origin Resource Sharing (CORS) requests for the route. |
+| `requireAuth` | boolean | false | A flag which tells the route function whether it should require an authorized user. If true, the deserialized User object will be available to the handler function with `req.user`. |
+| `bodyValidation` | object | false | A [Joi](https://npmjs.com/package/joi) validation scheme which will be applied to the request body. Access the validated object with `req.validatedBody` in the handler function. |
+| `queryValidation` | object | false | A [Joi](https://npmjs.com/package/joi) validation scheme which will be applied to the request querystring. Access the validated object with `req.validatedQuery` in the handler function. |
+| `paramValidation` | object | false | A [Joi](https://npmjs.com/package/joi) validation scheme which will be applied to the request url parameters. Access the validated object with `req.validatedParams` in the handler function. |
+| `validateShopifyRequest` | boolean | false | A flag which tells the route function whether it should validate the request as a Shopify request. |
+| `validateShopifyWebhook` | boolean | false | A flag which tells the route function whether it should validate the request as a Shopify webhook. |
+| `validateShopifyProxyPage` | boolean | false | A flag which tells the route function whether it should validate the request as a Shopify proxy page request. |
+
+## TypeScript interfaces
+
+This package comes complete with full TypeScript definitions! When using the `getRouter` function, you're expected to pass in the type interface for your User object. That will then give you intellisense on the `sealable_user_props` configuration option, and the `req.user` object in your route handlers.
+
+```ts
+import getRouter from "gearworks-route";
+
+interface User {
+    _id: string;
+    username: string;
+    shopify_access_token: string;
+}
+
+const route = getRouter<User>(expressApp, {
+    sealable_users_props: ["shopify_access_token" /* Array only accepts keys from the User interface */],
+    ...
+})
+
+route({
+    label: "Get home page",
+    method: "get",
+    path: "/home",
+    requireAuth: true,
+    handler: async function (req, res, next) {
+        // req.user is type User
+
+        ...
+    }
+})
+```
---
+++ index.ts
@@ -0,0 +1,271 @@
+import * as joi from "joi";
+import * as boom from "boom";
+import * as cors from "cors";
+import inspect from "logspect";
+import * as Bluebird from "bluebird";
+import { Auth } from "shopify-prime";
+import { seal, unseal } from "iron-async";
+import { decode, encode } from "jwt-simple";
+import { Express, Request, Response, NextFunction } from "express";
+import { json as parseJson, urlencoded as parseUrlEncoded } from "body-parser";
+
+export interface RouterRequest<UserType> extends Request {
+    user?: UserType;
+    validatedBody?: any;
+    validatedQuery?: any;
+    validatedParams?: any;
+    domainWithProtocol: string;
+}
+
+export type WithSessionTokenFunction<UserType> = (user: UserType, expInDays?: number) => Promise<RouterResponse<UserType>>;
+
+export interface RouterResponse<UserType> extends Response {
+    withSessionToken: WithSessionTokenFunction<UserType>;
+}
+
+export interface RouterFunctionConfig<UserType> {
+    method: "get" | "post" | "put" | "delete" | "head" | "all",
+    path: string,
+    handler: (req: RouterRequest<UserType>, res: RouterResponse<UserType>, next: NextFunction) => void | any,
+    label?: string,
+    cors?: boolean,
+    requireAuth?: boolean,
+    bodyValidation?: joi.Schema,
+    queryValidation?: joi.Schema,
+    paramValidation?: joi.Schema,
+    validateShopifyRequest?: boolean;
+    validateShopifyWebhook?: boolean;
+    validateShopifyProxyPage?: boolean;
+}
+
+export type RouterFunction<UserType> = (config: RouterFunctionConfig<UserType>) => void;
+
+export interface Config<UserType> {
+    iron_password: string;
+    jwt_secret_key: string;
+    shopify_secret_key: string;
+    auth_header_name?: string;
+    sealable_user_props?: (keyof UserType)[];
+    userAuthIsValid?: (user) => Promise<boolean>;
+}
+
+export default function getRouter<UserType>(app: Express, config: Config<UserType>) {
+    // Add configuration defaults
+    config = {
+        auth_header_name: "gearworks_auth",
+        sealable_user_props: [],
+        userAuthIsValid: async (user) => true,
+        ...config,
+    }
+
+    if (!config.iron_password) {
+        const error = new Error(`gearworks-route: iron_password is required in configuration object. Encryption and decryption is impossible.`);
+
+        throw error;
+    }
+
+    if (!config.jwt_secret_key) {
+        const error = new Error(`gearworks-route: jwt_secret_key is required in configuration object. Signing JWT tokens will be impossible.`);
+
+        throw error;
+    }
+
+    if (!config.shopify_secret_key) {
+        const error = new Error(`gearworks-route: shopify_secret_key is required in configuration object. Validating Shopify requests will be impossible.`);
+
+        throw error;
+    }
+
+    // Custom functions for Express request and response objects
+    const withSessionToken: WithSessionTokenFunction<UserType> = async function (this: RouterResponse<UserType>, user: UserType, expInDays = 30) {
+        // Encrypt any sensitive properties (access tokens, api keys, etc.) with Iron.
+        const sealedProps = await Bluebird.reduce(config.sealable_user_props, async (result, propName: string) => {
+            if (!!user[propName]) {
+                try {
+                    result[propName] = await seal(user[propName], config.iron_password);
+                } catch (e) {
+                    inspect(`Failed to encrypt Iron-sealed property ${propName}. Removing property from resulting session token object.`, e);
+
+                    // Prevent sending the unencrypted value to the client.
+                    result[propName] = undefined;
+                }
+            }
+
+            return result;
+        }, {});
+
+        // exp: Part of the jwt spec, specifies an expiration date for the token.
+        const exp = Date.now() + (expInDays * 24 * 60 * 60 * 1000);
+        const session = {
+            ...user as any,
+            exp,
+            ...sealedProps,
+        }
+
+        return this.json({ token: encode(session, config.jwt_secret_key) }) as RouterResponse<UserType>;
+    };
+
+    // Shim the app.response and app.request objects with our custom functions
+    app.response["withSessionToken"] = withSessionToken;
+
+    // A custom routing function that handles authentication and body/query/param validation
+    const route: RouterFunction<UserType> = (routeConfig) => {
+        const method = routeConfig.method.toLowerCase();
+        const corsMiddleware = routeConfig.cors ? cors() : (req, res, next) => next();
+        let jsonParserMiddleware = (req, res, next) => next();
+        let formParserMiddleware = (req, res, next) => next();
+
+        if (routeConfig.cors && routeConfig.method !== "all") {
+            // Add an OPTIONS request handler for the path. All non-trivial CORS requests from browsers 
+            // send an OPTIONS preflight request.
+            app.options(routeConfig.path, cors());
+        }
+
+        // Webhook validation must read the body exactly as its sent by Shopify, which is impossible when using parser middleware.
+        // If the route requires validation a Shopify webhook, we'll skip parser middleware and parse it ourselves.
+        if (!routeConfig.validateShopifyWebhook) {
+            // Set up request body parsers
+            jsonParserMiddleware = parseJson();
+            formParserMiddleware = parseUrlEncoded({ extended: true });
+        }
+
+        app[method](routeConfig.path, corsMiddleware, jsonParserMiddleware, formParserMiddleware, async function (req: RouterRequest<UserType>, res: RouterResponse<UserType>, next: NextFunction) {
+            req.domainWithProtocol = `${req.protocol}://${req.hostname}` + (req.hostname === "localhost" ? ":3000" : "");
+
+            if (routeConfig.requireAuth) {
+                const header = req.header(config.auth_header_name || "gearworks_auth");
+                let user;
+
+                try {
+                    user = decode(header, config.jwt_secret_key);
+                } catch (e) {
+                    return next(boom.unauthorized(`Missing or invalid ${config.auth_header_name || "gearworks_auth"} header.`));
+                }
+
+                // Ensure the decoded object is a user
+                if (!user) {
+                    return next(boom.unauthorized(`Decoded JWT token does not appear to be a valid user object.`));
+                }
+
+                // If user id exists in invalidation cache, return a 401 unauthed response.
+                try {
+                    const authIsValid = await config.userAuthIsValid(user);
+
+                    if (!authIsValid) {
+                        return next(boom.unauthorized(`userAuthIsValid function indicates that user's JWT token is no longer valid.`));
+                    }
+                } catch (e) {
+                    inspect(`Error attempting to check if user's auth is valid. Assuming true.`, e);
+                }
+
+                // Decrypt sensitive Iron-sealed properties
+                const unsealedProps = await Bluebird.reduce(config.sealable_user_props, async (result, propName: string) => {
+                    if (!!user[propName]) {
+                        try {
+                            result[propName] = await unseal(user[propName], config.jwt_secret_key);
+                        } catch (e) {
+                            inspect(`Failed to decrypt Iron-sealed property ${propName}.`, e);
+                        }
+                    }
+
+                    return result;
+                }, {});
+
+                req.user = Object.assign(user, unsealedProps);
+            };
+
+            if (routeConfig.bodyValidation) {
+                const validation = joi.validate(req.body, routeConfig.bodyValidation);
+
+                if (validation.error) {
+                    const error = boom.badData(validation.error.message, validation.error.details);
+
+                    return next(error);
+                }
+
+                req.validatedBody = validation.value;
+            }
+
+            if (routeConfig.queryValidation) {
+                const validation = joi.validate(req.query, routeConfig.queryValidation);
+
+                if (validation.error) {
+                    const error = boom.badData(validation.error.message, validation.error.details);
+
+                    return next(error);
+                }
+
+                req.validatedQuery = validation.value;
+            }
+
+            if (routeConfig.paramValidation) {
+                const validation = joi.validate(req.params, routeConfig.paramValidation);
+
+                if (validation.error) {
+                    const error = boom.badData(validation.error.message, validation.error.details);
+
+                    return next(error);
+                }
+
+                req.validatedParams = validation.value;
+            }
+
+            if (routeConfig.validateShopifyRequest) {
+                const isValid = await Auth.isAuthenticRequest(req.query, config.shopify_secret_key);
+
+                if (!isValid) {
+                    const error = boom.forbidden("Request does not pass Shopify's request validation scheme.");
+
+                    return next(error);
+                }
+            }
+
+            if (routeConfig.validateShopifyWebhook) {
+                // If the body is empty, there's no way to validate the webhook.
+                if (req.headers['transfer-encoding'] === undefined && isNaN(req.headers['content-length'] as any)) {
+                    const error = boom.forbidden("Request does not pass Shopify's webhook validation scheme.");
+                    inspect("Webhook body appears to be empty and cannot be validated. Headers:", req.headers);
+
+                    return next(error);
+                }
+
+                // To validate a webhook request, we must read the raw body as it was sent by Shopify — not the parsed body.
+                const rawBody = await new Bluebird<string>((res, rej) => {
+                    let body: string = "";
+
+                    req.on("data", chunk => body += chunk);
+                    req.on("end", () => res(body));
+                })
+
+                const isValid = await Auth.isAuthenticWebhook(req.headers, rawBody, config.shopify_secret_key);
+
+                if (!isValid) {
+                    const error = boom.forbidden("Request does not pass Shopify's webhook validation scheme.")
+
+                    return next(error);
+                }
+
+                if (req.header("content-type") === "application/json") {
+                    req.body = JSON.parse(rawBody);
+                }
+            }
+
+            if (routeConfig.validateShopifyProxyPage) {
+                const isValid = await Auth.isAuthenticProxyRequest(req.query, config.shopify_secret_key);
+
+                if (!isValid) {
+                    const error = boom.forbidden("Request does not pass Shopify's proxy page validation scheme.");
+
+                    return next(error);
+                }
+            }
+
+            // Pass control to the route's handler. Handlers can be async, so wrap them in a bluebird resolve which can catch unhandled promise rejections.
+            Bluebird.resolve(routeConfig.handler(req, res, next)).catch(e => {
+                return next(e);
+            });
+        });
+    }
+
+    return route;
+}
